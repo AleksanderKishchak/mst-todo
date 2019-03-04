271e923243519cbbc2f309cb4207cf5b6d38b0f6
Aleksander Kishchak <sanya69876@gmail.com>
2019-03-04 18:43:40 +0200

    Updates

---
+++ precache-manifest.8dab8c954639c0199fd4ba9ca7a13f3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4b18eb2bb927fe953b6",
+    "url": "/mst-todo/static/js/runtime~main.b4b18eb2.js"
+  },
+  {
+    "revision": "e814595099d0f3a8184f",
+    "url": "/mst-todo/static/js/main.e8145950.chunk.js"
+  },
+  {
+    "revision": "3ef127bed1189b1a2d57",
+    "url": "/mst-todo/static/js/2.3ef127be.chunk.js"
+  },
+  {
+    "revision": "e814595099d0f3a8184f",
+    "url": "/mst-todo/static/css/main.a2ba7b1f.chunk.css"
+  },
+  {
+    "revision": "25048184798b0289d5e0b66289ea8f17",
+    "url": "/mst-todo/index.html"
+  }
+];
